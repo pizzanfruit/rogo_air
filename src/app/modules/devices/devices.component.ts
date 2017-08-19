c5373e8fef22c92ec78f50f8eb08f307d64819c0
LongTH9 <longth9@fsoft.com.vn>
2017-08-19 23:59:52 +0700

    finish UI devices screen

--- src/app/modules/devices/devices.component.ts
+++ src/app/modules/devices/devices.component.ts
@@ -13,11 +13,23 @@ declare var $: any;
 
 export class DevicesComponent implements OnInit {
 
+  devices: any[] = [
+    {mac:"1", name: "Rogo Air 1", location: "FPT Shop tiểu khu Mỹ Lâm", temp: "29", humidity: "65", status: "Running"},
+    {mac:"2", name: "Rogo Air 2", location: "FPT Shop tiểu khu Mỹ Lâm", temp: "29", humidity: "65", status: "Running"},
+    {mac:"3", name: "Rogo Air 3", location: "FPT Shop tiểu khu Mỹ Lâm", temp: "29", humidity: "65", status: "Stopped"},
+  ];
+
   constructor(
-    private title: Title
+    private title: Title,
+    private router: Router
   ) { }
 
   ngOnInit() {
-
+    this.title.setTitle("Devices");
   }
+
+  selectDevice(mac: number) {
+    this.router.navigate(["tabs/devices", mac]);
+  }
+
 }
